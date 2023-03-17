f157f29862b811535cf706c19d898896edd38be1
Julius Koskela <julius.koskela@unikie.com>
2023-03-17 13:04:31 +0200

    Remove redundant comments
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- vmd-server/src/main.rs
+++ vmd-server/src/main.rs
@@ -19,20 +19,8 @@ fn kvm_api_version() -> String {
 
 #[launch]
 fn rocket() -> _ {
-    // See `Rocket.toml` and `Cargo.toml` for TLS configuration.
-    // Run `./private/gen_certs.sh` to generate a CA and key pairs.
     rocket::build().mount("/", routes![
         kvm_api_version,
         mutual
     ])
 }
-// fn main() {
-//     match kvm_util::check_kvm_api_version() {
-//         Ok(version) => println!("KVM API version: {}", version),
-//         Err(e) => eprintln!("Error: {:?}", e),
-//     }
-//     match kvm_util::check_kvm_extensions() {
-//         Ok(()) => println!("KVM extensions OK"),
-//         Err(e) => eprintln!("Error: {:?}", e),
-//     }
-// }
