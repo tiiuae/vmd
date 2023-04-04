1abcf545bd37fa8d92eef33b7f30cdb1dc2649f2
Julius Koskela <julius.koskela@unikie.com>
2023-04-04 12:24:30 +0300

    Add preliminary server code
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

---
+++ vmd-server/src/server.rs
@@ -0,0 +1,141 @@
+//! Main library entry point for vmd_api implementation.
+
+#![allow(unused_imports)]
+
+use async_trait::async_trait;
+use futures::{future, Stream, StreamExt, TryFutureExt, TryStreamExt};
+use hyper::server::conn::Http;
+use hyper::service::Service;
+use log::info;
+use std::future::Future;
+use std::marker::PhantomData;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+use swagger::{Has, XSpanIdString};
+use swagger::auth::MakeAllowAllAuthenticator;
+use swagger::EmptyContext;
+use tokio::net::TcpListener;
+
+#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
+use openssl::ssl::{Ssl, SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod};
+
+use vmd_api::models;
+
+/// Builds an SSL implementation for Simple HTTPS from some hard-coded file names
+pub async fn create(addr: &str, https: bool) {
+    let addr = addr.parse().expect("Failed to parse bind address");
+
+    let server = Server::new();
+
+    let service = MakeService::new(server);
+
+    let service = MakeAllowAllAuthenticator::new(service, "cosmo");
+
+    #[allow(unused_mut)]
+    let mut service =
+        vmd_api::server::context::MakeAddContext::<_, EmptyContext>::new(
+            service
+        );
+
+    if https {
+        #[cfg(any(target_os = "macos", target_os = "windows", target_os = "ios"))]
+        {
+            unimplemented!("SSL is not implemented for the examples on MacOS, Windows or iOS");
+        }
+
+        #[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
+        {
+            let mut ssl = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls()).expect("Failed to create SSL Acceptor");
+
+            // Server authentication
+            ssl.set_private_key_file("examples/server-key.pem", SslFiletype::PEM).expect("Failed to set private key");
+            ssl.set_certificate_chain_file("examples/server-chain.pem").expect("Failed to set certificate chain");
+            ssl.check_private_key().expect("Failed to check private key");
+
+            let tls_acceptor = ssl.build();
+            let tcp_listener = TcpListener::bind(&addr).await.unwrap();
+
+            loop {
+                if let Ok((tcp, _)) = tcp_listener.accept().await {
+                    let ssl = Ssl::new(tls_acceptor.context()).unwrap();
+                    let addr = tcp.peer_addr().expect("Unable to get remote address");
+                    let service = service.call(addr);
+
+                    tokio::spawn(async move {
+                        let tls = tokio_openssl::SslStream::new(ssl, tcp).map_err(|_| ())?;
+                        let service = service.await.map_err(|_| ())?;
+
+                        Http::new()
+                            .serve_connection(tls, service)
+                            .await
+                            .map_err(|_| ())
+                    });
+                }
+            }
+        }
+    } else {
+        // Using HTTP
+        hyper::server::Server::bind(&addr).serve(service).await.unwrap()
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Server<C> {
+    marker: PhantomData<C>,
+}
+
+impl<C> Server<C> {
+    pub fn new() -> Self {
+        Server{marker: PhantomData}
+    }
+}
+
+
+use vmd_api::{
+    Api,
+    GetVmInfoByIdResponse,
+    GetVmListResponse,
+    VmActionResponse,
+};
+use vmd_api::server::MakeService;
+use std::error::Error;
+use swagger::ApiError;
+
+#[async_trait]
+impl<C> Api<C> for Server<C> where C: Has<XSpanIdString> + Send + Sync
+{
+    /// Gets virtual machine info by ID
+    async fn get_vm_info_by_id(
+        &self,
+        id: serde_json::Value,
+        context: &C) -> Result<GetVmInfoByIdResponse, ApiError>
+    {
+        let context = context.clone();
+        info!("get_vm_info_by_id({:?}) - X-Span-ID: {:?}", id, context.get().0.clone());
+        Err(ApiError("Generic failure".into()))
+    }
+
+    /// Get list IDs for all virtual machines
+    async fn get_vm_list(
+        &self,
+        context: &C) -> Result<GetVmListResponse, ApiError>
+    {
+        let context = context.clone();
+        info!("get_vm_list() - X-Span-ID: {:?}", context.get().0.clone());
+        Err(ApiError("Generic failure".into()))
+    }
+
+    /// Request to perform a control action on the virtual machine by its ID
+    async fn vm_action(
+        &self,
+        action: serde_json::Value,
+        id: serde_json::Value,
+        context: &C) -> Result<VmActionResponse, ApiError>
+    {
+        let context = context.clone();
+        info!("vm_action({:?}, {:?}) - X-Span-ID: {:?}", action, id, context.get().0.clone());
+        Err(ApiError("Generic failure".into()))
+    }
+
+}
