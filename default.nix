310d552ca339cba0643c3c897af25fa08295a8d4
Julius Koskela <julius.koskela@unikie.com>
2023-05-04 13:49:56 +0300

    Move dependencies to  in
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- default.nix
+++ default.nix
@@ -1,24 +1,22 @@
 let
-  pkgs = import <nixpkgs> {};
-in
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "vmd";
-  version = "0.0.1";
+	pkgs = import <nixpkgs> {};
+in pkgs.rustPlatform.buildRustPackage rec {
+	pname = "vmd";
+	version = "0.0.1";
+	cargoLock.lockFile = ./Cargo.lock;
+	PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
-  src = ./.;
+	src = ./.;
 
-  buildInputs = with pkgs; [
-    pkg-config
-    openssl
-    openssl.dev
-	openapi-generator-cli
-	gnumake
-  ];
+	nativeBuildInputs = with pkgs; [
+		pkg-config
+		openssl.dev
+		openapi-generator-cli
+		gnumake
+	];
 
-  configurePhase = ''
-    make generate-server-api;
-	make generate-client-api;
-  '';
-  cargoLock.lockFile = ./Cargo.lock;
-  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+	configurePhase = ''
+		make generate-server-api;
+		make generate-client-api;
+	'';
 }
