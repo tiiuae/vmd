e94541db24e9057d707ce702310d6db5049d2fc9
Julius Koskela <julius.koskela@unikie.com>
2023-05-08 14:02:32 +0300

    Refactor
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- flake.nix
+++ flake.nix
@@ -14,44 +14,54 @@
 
   outputs = { self, nixpkgs, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        craneLib = crane.lib.${system};
-        my-crate = craneLib.buildPackage {
-			pname = "vmd";
-			version = "0.1.0";
-        	src = craneLib.cleanCargoSource (craneLib.path ./.);
-
-			nativeBuildInputs = with pkgs; [
-				gnumake
-				pkg-config
-				openapi-generator-cli
-				openssl.dev
-				rustc
-				cargo
-			];
-
-			PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        }; in {
-			checks = {
-				inherit my-crate;
+		let
+			pkgs = import nixpkgs {
+				inherit system;
 			};
 
-			packages.default = my-crate;
+			craneLib = crane.lib.${system};
+			package = craneLib.buildPackage {
+				pname = "vmd";
+				version = "0.1.0";
+				src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+				nativeBuildInputs = with pkgs; [
+					gnumake
+					pkg-config
+					openapi-generator-cli
+					openssl.dev
+					rustc
+					cargo
+				];
+
+				PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+			};
+		in {
+			checks = {
+				inherit package;
+			};
+
+			packages.default = package;
 
 			apps.default = flake-utils.lib.mkApp {
-				drv = my-crate;
+				drv = package;
 			};
 
 			devShells.default = pkgs.mkShell {
 				inputsFrom = builtins.attrValues self.checks.${system};
 
-				inherit my-crate;
+				nativeBuildInputs = with pkgs; [
+					gnumake
+					pkg-config
+					openapi-generator-cli
+					openssl.dev
+					rustc
+					cargo
+				];
+
+				PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 			};
-    	}
+		}
 	);
 }
 
