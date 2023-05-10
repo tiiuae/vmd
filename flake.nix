e309c277f1aa5a300be515c881c88eb5844eab28
Julius Koskela <julius.koskela@unikie.com>
2023-05-10 15:19:14 +0300

    Add formatter and format flake.nix
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- flake.nix
+++ flake.nix
@@ -5,64 +5,70 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     crane = {
-		url = "github:ipetkov/crane";
-		inputs.nixpkgs.follows = "nixpkgs";
-		inputs.flake-utils.follows = "flake-utils";
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-		let
-			pkgs = import nixpkgs {
-				inherit system;
-			};
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-			craneLib = crane.lib.${system};
-			package = craneLib.buildPackage {
-				pname = "vmd";
-				version = "0.1.0";
-				src = craneLib.cleanCargoSource (craneLib.path ./.);
+        craneLib = crane.lib.${system};
+        package = craneLib.buildPackage {
+          pname = "vmd";
+          version = "0.1.0";
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-				nativeBuildInputs = with pkgs; [
-					gnumake
-					pkg-config
-					openapi-generator-cli
-					openssl.dev
-					rustc
-					cargo
-				];
+          nativeBuildInputs = with pkgs; [
+            gnumake
+            pkg-config
+            openapi-generator-cli
+            openssl.dev
+            rustc
+            cargo
+          ];
 
-				PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-			};
-		in {
-			checks = {
-				inherit package;
-			};
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      in {
+        checks = {
+          inherit package;
+        };
 
-			packages.default = package;
+        packages.default = package;
+        formatter = nixpkgs.legacyPackages.${system}.alejandra;
 
-			apps.default = flake-utils.lib.mkApp {
-				drv = package;
-			};
+        apps.default = flake-utils.lib.mkApp {
+          drv = package;
+        };
 
-			devShells.default = pkgs.mkShell {
-				inputsFrom = builtins.attrValues self.checks.${system};
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks.${system};
 
-				nativeBuildInputs = with pkgs; [
-					gnumake
-					pkg-config
-					openapi-generator-cli
-					openssl.dev
-					rustc
-					cargo
-				];
+          nativeBuildInputs = with pkgs; [
+            gnumake
+            pkg-config
+            openapi-generator-cli
+            openssl.dev
+            rustc
+            cargo
+          ];
 
-				PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-			};
-		}
-	);
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      }
+    );
 }
-
